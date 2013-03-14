0f04a303b76e8539913ab4fbe955f422ccd79e4c
leobabauta <leo.babauta@gmail.com>
2013-03-13 20:02:36 -0700

    tried to fix construct, not done yet

--- index.php
+++ index.php
@@ -9,48 +9,14 @@ class Traveler {
 	public $Age;
 	public $Sex;
 	public $table = "travelers";
-	// these are used in load and toString fuctions
-	public $resultFirst;
-	public $resultLast;
-	public $resultAge;
-	public $resultSex;
-	public $fullSex;
 
-	// uses a series of constructs depending on how many parameters are passed to Traveler class
-	// this first one decides how many parameters there are, and calls the appropriate subconstruct
- 	public function __construct() { 
-        $a = func_get_args(); 
-        $i = func_num_args(); 
-        if (method_exists($this,$f='__construct'.$i)) { 
-            call_user_func_array(array($this,$f),$a); 
-        } 
-    } 
-    
-    public function __construct1($a1) { 
-    	// for if there is just one parameter
-  		$this->FirstName = $a1;
-    } 
-    
-    public function __construct2($a1,$a2) { 
-    	// for if there are 2 parameters
-  		$this->FirstName = $a1;
-  		$this->LastName = $a2;
-    } 
-    
-    public function __construct3($a1,$a2,$a3) { 
-    	// for if there are 3 parameters
-  		$this->FirstName = $a1;
-  		$this->LasttName = $a2;
-  		$this->Age = $a3;
-  	} 
-    
-    public function __construct4($a1,$a2,$a3, $a4) { 
-    	// for if there are 4 parameters
-  		$this->FirstName = $a1;
-  		$this->LastName = $a2;
-  		$this->Age = $a3;
-  		$this->Sex = $a4;
-    } 
+	// puts parameters into an array, then assigns values to variables from array
+ 	public function __construct($params = Array()) {
+        $this->FirstName = isset($params['FirstName']) ? $params['FirstName'] : null;
+        $this->LastName = isset($params['LastName']) ? $params['LastName'] : null;
+        $this->Age = isset($params['Age']) ? $params['Age'] : null;
+        $this->Sex = isset($params['Sex']) ? $params['Sex'] : null;
+    }
 
   	public function save() {
   		global $link;
@@ -100,30 +66,33 @@ class Traveler {
 
 		// next puts the result of the query into an array, then into individual variables
 		$resultArray = mysqli_fetch_array($result);
-		$this->resultFirst = $resultArray['FirstName'];
-		$this->resultLast = $resultArray['LastName'];
-		$this->resultAge = $resultArray['Age'];
-		$this->resultSex = $resultArray['Sex'];
-
-		// change sex to full word
-		if ($this->resultSex == 'M') {
-			$this->fullSex = 'Male';
-		} elseif ($this->resultSex == 'F') {
-			$this->fullSex = 'Female';
-		}
+		$this->FirstName = $resultArray['FirstName'];
+		$this->LastName = $resultArray['LastName'];
+		$this->Age = $resultArray['Age'];
+		$this->Sex = $resultArray['Sex'];
 	}
 
  	public function __toString() {
+		// test this->sex variable
+ 		echo "this->sex variable is " . $this->Sex . "<br />";
+
+		// change sex to full word
+		if ($this->Sex == 'M') {
+			$fullSex = 'Male';
+		} elseif ($this->Sex == 'F') {
+			$fullSex = 'Female';
+		}
+
 		// then create string to return when class is echoed as a string
-        return $this->resultFirst . " " . $this->resultLast . ", Age " . $this->resultAge . ", " . $this->fullSex . "<br />";
+        return $this->FirstName . " " . $this->LastName . ", Age " . $this->Age . ", " . $fullSex . "<br />";
     }
 
 }
 
-$traveler1 = new Traveler("Maia","Cruz","13","F");
+$traveler1 = new Traveler("Amy","Cruz","33","F");
 $traveler1->save();
 
-$traveler2 = new Traveler("Maia","Cruz");
+$traveler2 = new Traveler("Amy","Cruz");
 $traveler2->load();
 
 echo $traveler2;
